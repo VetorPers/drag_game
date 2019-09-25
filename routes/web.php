828c37dd22723c062e225ee39cd8d89eeeb93493
VetorPers <506969463@qq.com>
2019-09-25 16:16:23 +0800

    tz

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,12 @@
 //Auth::routes();
 
 Route::get('/', 'PestController@index')->name('home');
+Route::get('detail', 'PestController@index');
+Route::get('chooseGame', 'PestController@index');
+Route::get('startGame', 'PestController@index');
+Route::get('playGame', 'PestController@index');
+Route::get('over', 'PestController@index');
+
 Route::post('login', 'PestController@loginPost');
 Route::post('pest', 'PestController@pest');
 Route::post('pestInfo', 'PestController@pestInfo');
